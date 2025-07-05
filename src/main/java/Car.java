5278034947749100426bd95116031ea594e5ccaa
tahurun009 <gurdin009@gmail.com>
2025-07-05 15:15:05 +0300

    My Commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,9 @@
+class Car {
+    String title;
+    int speed;
+
+    Car(String title, int speed) {
+        this.title = title;
+        this.speed = speed;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Создаём экземпляр класса Scanner.
+        // Используем один экземпляр класса во всей программе.
+        Scanner scanner = new Scanner(System.in);
+        String carTitle = "";
+        int carSpeed = 0;
+        ArrayList<Car> cars = new ArrayList<>();
+
+        // Запрашиваем и читаем название и скорость для трёх автомобилей
+        for (int numCar = 1; numCar <= 3; numCar++) {
+            try {
+                System.out.println("— Введите название машины №" + numCar + ":");
+                carTitle = scanner.next().trim();
+
+            } catch (Exception e) {
+                System.out.println("Введите корректные данные");
+            }
+            while (true) {
+                try {
+                    System.out.println("— Введите скорость машины №" + numCar + ": ");
+                    if (scanner.hasNextInt()) {
+                        carSpeed = scanner.nextInt();
+                        if (carSpeed >= 0 && carSpeed <= 250) {
+                            break;
+                        }
+                        System.out.println("— Неправильная скорость ");
+                    } else {
+                        System.out.println("— Неправильная скорость ");
+                        scanner.next();
+                    }
+                } catch (Exception e) {
+                    System.out.println("— Неправильная скорость ");
+                }
+
+            }
+            cars.add(new Car(carTitle, carSpeed));
+        }
+
+        String leaderTitleInRace = Race.leader(cars);
+        System.out.println("Самая быстрая машина: " + leaderTitleInRace);
+
     }
 }
